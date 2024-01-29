076560d72a97dabe8fb4a89bb73763f5cd41f34e
Destray32 <kubaj38@gmail.com>
2024-01-29 13:22:21 +0100

    init

---
+++ main.py
@@ -0,0 +1,8 @@
+# this file will be responsible for importing other classes and libraries that will be essantial for the program to run
+
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
